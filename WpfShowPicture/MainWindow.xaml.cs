af08c4c767457d6901ba2baa349d99914db8c566
Admin <Admin@X>
2018-05-10 23:31:16 +0300

    Finished project

--- WpfShowPicture/MainWindow.xaml.cs
+++ WpfShowPicture/MainWindow.xaml.cs
@@ -23,14 +23,18 @@ namespace WpfShowPicture
     public partial class MainWindow : Window
     {
         private delegate void UpdateProgressBarDelegate(System.Windows.DependencyProperty dp, Object value);
-        List<string> listPicture;
-        List<string> masPicture;
-        List<double> masSizeImg;
-        int count = 0;
+        List<string> listPicture; // colection exts
+        List<string> masPicture; // array Img
+        //List<double> masSizeImg; // array height/width or width/height
+        int count = 0; // count img
         double value = 0;
         Image mainImg;
         StackPanel tempStPanel;
         int ob = 0; // номер объекта на который пользователь кликнул в StPanel
+        bool isHeightBigger { get; set; } = false;
+        List<double> mass1; // height img
+        List<double> mass2;// width img
+
         public MainWindow()
         {
             InitializeComponent();
@@ -41,13 +45,24 @@ namespace WpfShowPicture
             masPicture = new List<string>();
             tempStPanel = new StackPanel();
             mainImg = new Image();
-            masSizeImg = new List<double>();
             listPicture = new List<string>() { ".jpg", ".png", ".bmp" };
-            
+            mass1 = new List<double>();
+            mass2 = new List<double>();
         }
 
         private void btnOpenFolder_Click(object sender, RoutedEventArgs e)
         {
+            txtBPath.Text = "";
+            mass1.Clear();
+            mass2.Clear();
+            masPicture.Clear();
+            tempStPanel.Children.Clear();
+            lstBoxMainImage.Source = null;
+            mainImg.Source = null;
+            value = 0;
+            ob = 0;
+            sliderZoom.Value = 515;
+
             StackPanel stpanel = new StackPanel();
             stpanel.VerticalAlignment = VerticalAlignment.Top;
 
@@ -55,17 +70,21 @@ namespace WpfShowPicture
             System.Windows.Forms.DialogResult dlgRes = fbd.ShowDialog();
             if (!string.IsNullOrWhiteSpace(fbd.SelectedPath))
             {
-                string[] files = Directory.GetFiles(fbd.SelectedPath);
-                for (int i = 0; i < files.Length; i++)
+                try
                 {
-                    for (int j = 0; j < listPicture.Count; j++)
+                    string[] files = Directory.GetFiles(fbd.SelectedPath);
+                    for (int i = 0; i < files.Length; i++)
                     {
-                        if (files[i].Substring(files[i].Length - 4) == listPicture[j])
+                        for (int j = 0; j < listPicture.Count; j++)
                         {
-                            masPicture.Add(files[i]);
+                            if (files[i].Substring(files[i].Length - 4) == listPicture[j])
+                            {
+                                masPicture.Add(files[i]);
+                            }
                         }
                     }
                 }
+                catch { };
             }
 
             prgBarDownloadPict.Minimum = 0;
@@ -73,23 +92,22 @@ namespace WpfShowPicture
             prgBarDownloadPict.Value = 0;
             PrBarDownload(masPicture, stpanel);
 
-            //scrlViewerImg.Content = stpanel;
             AddClickToImg(stpanel);
+            //stpanel.Children.Clear();
         }
 
-       
-       
 
 
         private Button createImage(int count, string str)
         {
+            //masSizeImg.Clear();
             BitmapImage tempBmp = new BitmapImage();
             tempBmp.BeginInit();
             tempBmp.UriSource = new Uri(str);
             tempBmp.EndInit();
-            //double w = tempBmp.Width;
-            masSizeImg.Add(tempBmp.Height / tempBmp.Width);
-
+           
+            mass1.Add(tempBmp.Height);
+            mass2.Add(tempBmp.Width);
 
             Image tempImg = new Image();
             tempImg.Margin = new Thickness(5);
@@ -110,28 +128,37 @@ namespace WpfShowPicture
 
         private void PrBarDownload(List<string> masPicture, StackPanel stpanel)
         {
+            count = 0;
             tempStPanel = null;
             UpdateProgressBarDelegate updatePbDelegate = new UpdateProgressBarDelegate(prgBarDownloadPict.SetValue);
 
-                //Tight Loop:  Loop until the ProgressBar.Value reaches the max
-              do
-              {
-                scrlViewerImg.Content = null;
-                value += 1;
-                stpanel.Children.Add(createImage(count, masPicture[count]));
-                prgBarDownloadPict.Value = count + 1;
-                count++;
-                    /*Update the Value of the ProgressBar:
-                      1)  Pass the "updatePbDelegate" delegate that points to the ProgressBar1.SetValue method
-                      2)  Set the DispatcherPriority to "Background"
-                      3)  Pass an Object() Array containing the property to update (ProgressBar.ValueProperty) and the new value */
-                Dispatcher.Invoke(updatePbDelegate,
-                    System.Windows.Threading.DispatcherPriority.Background, new object[] { ProgressBar.ValueProperty, value });
+            Border borderStPanel = new Border();
+            borderStPanel.Margin = new Thickness(3);
+            borderStPanel.BorderBrush = Brushes.Black;
+            borderStPanel.BorderThickness = new Thickness(2);
+            borderStPanel.CornerRadius = new CornerRadius(5);
+            if (masPicture.Count != 0)
+            {
+                do
+                {
+                    borderStPanel.Child = null;
+                    scrlViewerImg.Content = null;
+                    value++;//= 1;
 
-                scrlViewerImg.Content = stpanel;
-              } while (prgBarDownloadPict.Value != prgBarDownloadPict.Maximum);
+                    stpanel.Children.Add(createImage(count, masPicture[count]));
+                    scrlViewerImg.Content = borderStPanel;
+                    borderStPanel.Child = stpanel;
+                    prgBarDownloadPict.Value = count++;// 1;
+                                                       //count++;
+                    Dispatcher.Invoke(updatePbDelegate,
+                      System.Windows.Threading.DispatcherPriority.Background, new object[] { ProgressBar.ValueProperty, value });
 
+                    if (prgBarDownloadPict.Value == prgBarDownloadPict.Maximum)
+                        Thread.Sleep(500);
+                } while (prgBarDownloadPict.Value != prgBarDownloadPict.Maximum);
+            }
             tempStPanel = stpanel;
+            prgBarDownloadPict.Value = 0;
         }
 
 
@@ -147,32 +174,50 @@ namespace WpfShowPicture
             }
         }
 
+
         private void MainWindow_Click(object sender, RoutedEventArgs e)
         {
+            
+            txblExp.Text = "";
             Button tempBtn = new Button();
             tempBtn = sender as Button;
             for (int i = 0; i < tempStPanel.Children.Count; i++)
             {
                 if ((tempStPanel.Children[i] as Button).Name == tempBtn.Name)
                 {
+                    lstBoxMainImage.MaxHeight = mass1[i];
+                    lstBoxMainImage.MaxWidth = mass2[i];
+                    
                     BitmapImage tempBmp = new BitmapImage();
                     tempBmp.BeginInit();
                     tempBmp.UriSource = new Uri(masPicture[i]);
                     tempBmp.EndInit();
                     mainImg.Source = tempBmp;
                     ob = i;
+                    
                     txtBPath.Text = masPicture[i];
+                    FileInfo fInfo = new FileInfo(masPicture[i]);
+                    txblExp.Inlines.Add("Имя ф. - ");
+                    txblExp.Inlines.Add(new Run($"{fInfo.Name.ToString()}") {FontWeight = FontWeights.Bold });
+
+                    double dleng = fInfo.Length;
+                    dleng = dleng / (1024*1024);
+                    dleng = Math.Round(dleng, 1);
+                    txblExp.Inlines.Add(" Размер ф. - ");
+                    txblExp.Inlines.Add(new Run($"{ dleng.ToString()}") { FontWeight = FontWeights.Bold });
+
+                    txblExp.Inlines.Add("мб Тип ф. - ");
+                    txblExp.Inlines.Add(new Run($"{fInfo.Extension.ToString()}") { FontWeight = FontWeights.Bold });
+
+                    txblExp.Inlines.Add(" Дата создания - ");
+                    txblExp.Inlines.Add(new Run($"{fInfo.CreationTime.ToString()}") { FontWeight = FontWeights.Bold });
+
+                    txblExp.Inlines.Add(" Дата открытия - ");
+                    txblExp.Inlines.Add(new Run($"{fInfo.LastAccessTime.ToString()}") { FontWeight = FontWeights.Bold });
                 }
             }
-            //lstBoxMainImage.Items.Add(mainImg);
-            //lstboxItemImg.Content = mainImg;
+            lstBoxMainImage.Height = gridMainImg.Height;
             lstBoxMainImage.Source = mainImg.Source;
-        }
-
-        private void sliderZoom_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
-        {
-            if(lstBoxMainImage.Height > 0 && lstBoxMainImage.Width >0)
-                lstBoxMainImage.MaxHeight = lstBoxMainImage.Width * masSizeImg[ob];
         }
     }
 }
